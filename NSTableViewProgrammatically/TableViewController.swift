498d62abc453c92d5fa5fd4aa972f27ff217889c
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-28 12:43:19 +0300

    Start change initial code and puting own lines

--- NSTableViewProgrammatically/TableViewController.swift
+++ NSTableViewProgrammatically/TableViewController.swift
@@ -10,58 +10,84 @@
 
 import Cocoa
 
-let tableViewFrame = NSRect(x: 0, y: 0, width: 250, height: 500)
+//let tableViewFrame = NSRect(x: 0, y: 0, width: 100, height: 500)
 
+// MARK: - Data
+let days = ["I", "II", "III"]
+let hours = ["9:00 - 10:00", "11:00 - 12:00", "8:00 - 16:00"]
+
+// MARK: -
 class TableViewController: NSViewController {
-  let tableView: NSTableView = {
-    let tableView = NSTableView()
-
-    // By default a tableview has column spacing. We only have
-    // one column so this will the table from side scrolling
-    tableView.intercellSpacing = NSSize(width: 0, height: 2)
-    tableView.headerView = nil
-    return tableView
-  }()
-
-  let scrollView = NSScrollView()
-  let column = NSTableColumn()
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    tableView.dataSource = self
-    tableView.delegate = self
-    tableView.addTableColumn(column)
-
-    column.width = tableViewFrame.size.width
-
-    scrollView.documentView = tableView
-  }
-
-  // Loading a view controller without a NIB requires that we
-  // override loadView and provide our own view instead
-  override func loadView() {
-    view = scrollView
-  }
+    // MARK: - Properties
+    let scrollView = NSScrollView()
+    
+    let tableView: NSTableView = {
+        let tableView = NSTableView()
+        
+        // A table view defaults to setting the intercell spacing (the gaps between columns and rows) to (3.0, 2.0)
+        // To change it should be used:
+        //    tableView.intercellSpacing = NSSize(width: 0, height: 2)
+        
+        // Normally a table has a header view.
+        // To remove it use:
+        //    tableView.headerView = nil
+        return tableView
+    }()
+    
+    let column = NSTableColumn()
+    let column2 = NSTableColumn()
+    
+    
+    //    MARK: - Start here:
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        column.width = 100
+        //        column2.width = tableViewFrame.size.width
+        column2.width = 200
+        column2.title = days[2]
+        
+        
+        // Add collumn to table:
+        tableView.addTableColumn(column)
+        tableView.addTableColumn(column2)
+        
+        
+        
+        // Tell the scroll view about the table view,
+        // by setting the documentView to our table view:
+        scrollView.documentView = tableView
+    }
+    
+    // Loading a view controller without a NIB requires that we
+    // override loadView and provide our own view instead
+    override func loadView() {
+        view = scrollView
+    }
 }
 
+
+// MARK: - Extentions
 extension TableViewController: NSTableViewDataSource {
-  func numberOfRows(in tableView: NSTableView) -> Int {
-    return 30
-  }
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return days.count
+    }
 }
 
 extension TableViewController: NSTableViewDelegate {
-  func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
-    return 20
-  }
-
-  func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-    return NSTextField(labelWithString: "Cell \(row + 1)")
-  }
+    //    func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
+    //        return 20
+    //    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        
+        
+        
+        
+        return NSTextField(labelWithString: "Cell \(row + 1)")
+    }
 }
-//
-//let vc = TableViewController(nibName: nil, bundle: nil)
-//vc.view.frame = tableViewFrame
-//
-//PlaygroundPage.current.liveView = vc
