81e1d576be3feba01780011cc19127511e331ccb
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-28 08:13:58 +0300

    Initial Commit

---
+++ NSTableViewProgrammatically/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSTableViewProgrammatically
+//
+//  Created by Office-iMac on 2020-09-28.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NSTableViewProgrammatically/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSTableViewProgrammatically
+//
+//  Created by Office-iMac on 2020-09-28.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
