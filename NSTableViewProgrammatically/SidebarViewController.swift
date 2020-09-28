513861c1f8bc3115aaabd8d5231fd2ce826bd18c
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-28 08:24:37 +0300

    Create SidebarViewController file and asign it to ViewController

---
+++ NSTableViewProgrammatically/SidebarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SidebarViewController.swift
+//  NSTableViewProgrammatically
+//
+//  Created by Office-iMac on 2020-09-28.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import Cocoa
+
+class SidebarViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    
+    
+}
